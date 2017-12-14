35305a582affb2eea9930d05bbcfa31862178712
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-12-14 19:51:44 +0100

    Sync

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,49 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import MainLayout from '@/components/layouts/MainLayout'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
 import Example from '@/components/Example'
+import TodoView from '@/components/TodoView'
+import NotFoundView from '@/components/NotFoundView'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      component: Login
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: MainLayout,
+      children: [
+        {
+          path: 'hello',
+          alias: '',
+          component: HelloWorld,
+          name: 'HelloWorld',
+          meta: {description: 'Vue Hello World'}
+        },
+        {
+          path: 'example',
+          component: Example,
+          name: 'Example',
+          meta: {description: 'Example'}
+        },
+        {
+          path: 'todo',
+          component: TodoView,
+          name: 'TodoView',
+          meta: {description: 'TODO', requiresAuth: true}
+        }
+      ]
     },
     {
-      path: '/hello',
-      redirect: '/'
-    },
-    {
-      path: '/example',
-      name: 'Example',
-      component: Example
+      // not found handler
+      path: '*',
+      component: NotFoundView
     }
   ]
 })
